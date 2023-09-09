6f816fb3db3c84b7e1fccc63898473f5e611b97c
martadelka <oamedvedeva777@gmail.com>
2023-09-09 21:57:55 +0300

    js-hw6-2task

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,17 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const ingredientsList = document.querySelector("#ingredients");
+
+const array = [];
+ingredients.forEach(element => {
+  const liElem = document.createElement("li");
+  liElem.textContent = element;
+  liElem.classList.add("item");
+  array.push(liElem)
+});
+
+console.log(array);
+ingredientsList.append(...array);
+console.log(ingredientsList);
