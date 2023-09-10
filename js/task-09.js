fcd6eca2b53b5150aa5bc95c8330d9652a826f35
martadelka <oamedvedeva777@gmail.com>
2023-09-10 16:55:57 +0300

    js-hw6-task9

--- js/task-09.js
+++ js/task-09.js
@@ -3,3 +3,16 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+var spanColor = document.querySelector('.color');
+
+const setBg = () => {
+  const randomColor = Math.floor(Math.random()*16777215).toString(16);
+  document.body.style.backgroundColor = "#" + randomColor;
+  spanColor.innerHTML = "#" + randomColor;
+}
+
+const btnChange = document.querySelector('.change-color');
+
+btnChange.addEventListener("click", setBg);
+setBg();
