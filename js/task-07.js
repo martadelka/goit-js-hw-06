8c0ead5f6f9f6aee05d343c59a65759ea3cf96c9
martadelka <oamedvedeva777@gmail.com>
2023-09-10 16:25:34 +0300

    js-hw6-task8

--- js/task-07.js
+++ js/task-07.js
@@ -1,11 +1,9 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const fontSizeControl = document.querySelector('#font-size-control');
-    const textElement = document.querySelector('#text');
+const fontSizeControl = document.querySelector('#font-size-control');
+const textElement = document.querySelector('#text');
 
-    fontSizeControl.addEventListener('input', onInput);
+fontSizeControl.addEventListener('input', onInput);
 
-    function onInput() {
-        const fontSizeValue = this.value + 'px';
-      textElement.style.fontSize = fontSizeValue;
-    };
-  });
+function onInput() {
+    const fontSizeValue = this.value + 'px';
+    textElement.style.fontSize = fontSizeValue;
+};
--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,17 @@
+const formEl = document.querySelector('.login-form');
+formEl.addEventListener('submit', handleSubmit);
 
+function handleSubmit(event) {
+    event.preventDefault();
+    const { email, password } = event.currentTarget.elements;
+    if (email.value === "" || password.value === "") {
+        alert("Всі поля повинні бути заповнені.");
+    }
+
+    const data = {
+        email: email.value,
+        password: password.value,
+    }
+    console.dir(data)
+    formEl.reset();
+}
