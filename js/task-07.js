69996b479f6c5b18f28e47b3783a496c7e1e66a2
martadelka <oamedvedeva777@gmail.com>
2023-09-11 16:02:53 +0300

    js-hw6-1pravka

--- js/task-07.js
+++ js/task-07.js
@@ -1,7 +1,8 @@
 const fontSizeControl = document.querySelector('#font-size-control');
-const textElement = document.querySelector('#text');
+var textElement = document.querySelector('#text');
 
 fontSizeControl.addEventListener('input', onInput);
+textElement.style.fontSize = fontSizeControl.value;
 
 function onInput() {
     const fontSizeValue = this.value + 'px';
